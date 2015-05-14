d6261dff6d9b439f92fded6b4236d9ac6d41e8f4
Jan Kiszka <jan.kiszka@siemens.com>
2015-05-14 16:20:20 +0200

    tools, inmates: cell-linux: Pass PM timer address via setup_data
    Append a Jailhouse-specific setup_data structure to the zero-page and
    use it for telling Linux where the PM timer is located. The structure
    will be extended later to carry more information.

--- inmates/tools/x86/linux-loader.c
+++ inmates/tools/x86/linux-loader.c
@@ -12,24 +12,37 @@
 
 #include <inmate.h>
 
+#define ZERO_PAGE_ADDR		0xf5000UL
+
 struct boot_params {
 	u8	padding1[0x230];
 	u32	kernel_alignment;
-	u8	padding2[0x260 - 0x230 - 4];
+	u8	padding2[0x250 - 0x230 - 4];
+	u64	setup_data;
+	u8	padding3[8];
 	u32	init_size;
 };
 
-#define ZERO_PAGE_ADDR		0xf5000UL
+struct setup_data {
+	u64	next;
+	u32	type;
+	u32	length;
+	u16	pm_timer_address;
+};
 
 void inmate_main(void)
 {
-	struct boot_params *boot_params= (struct boot_params *)ZERO_PAGE_ADDR;
+	struct boot_params *boot_params = (struct boot_params *)ZERO_PAGE_ADDR;
 	void (*entry)(int, struct boot_params *);
+	struct setup_data *setup_data;
 	void *kernel;
 
 	kernel = (void *)(unsigned long)boot_params->kernel_alignment;
 
 	map_range(kernel, boot_params->init_size, MAP_CACHED);
+
+	setup_data = (struct setup_data *)boot_params->setup_data;
+	setup_data->pm_timer_address = comm_region->pm_timer_address;
 
 	entry = kernel + 0x200;
 	entry(0, boot_params);
