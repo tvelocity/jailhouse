0eae9cb6a1e6641c35f4d3e161f7b0520e87a85b
Antonios Motakis <antonios.motakis@huawei.com>
2015-07-01 10:21:30 +0200

    Add configuration for the ARMv8 Foundation model
    Add under config/foundation-v8.c a root cell configuration for the ARMv8
    Foundation model, so we can in the future use this target with Jailhouse.
    We also add the neccessary parameters in asm/platform.h for this model.
    
    Signed-off-by: Antonios Motakis <antonios.motakis@huawei.com>

--- hypervisor/arch/arm64/include/asm/platform.h
+++ hypervisor/arch/arm64/include/asm/platform.h
@@ -15,4 +15,37 @@
 
 #include <jailhouse/config.h>
 
+#ifdef CONFIG_MACH_FOUNDATION_V8
+
+# ifdef CONFIG_ARM_GIC_V3
+#  define GICD_BASE	((void *)0x2f000000)
+#  define GICD_SIZE	0x10000
+#  define GICR_BASE	((void *)0x2f100000)
+#  define GICR_SIZE	0x100000
+
+#  include <asm/gic_v3.h>
+# else /* GICv2 */
+#  define GICD_BASE	((void *)0x2c001000)
+#  define GICD_SIZE	0x1000
+#  define GICC_BASE	((void *)0x2c002000)
+/*
+ * WARN: most device trees are broken and report only one page for the GICC.
+ * It will brake the handle_irq code, since the GICC_DIR register is located at
+ * offset 0x1000...
+ */
+#  define GICC_SIZE	0x2000
+#  define GICH_BASE	((void *)0x2c004000)
+#  define GICH_SIZE	0x2000
+#  define GICV_BASE	((void *)0x2c006000)
+#  define GICV_SIZE	0x2000
+
+#  include <asm/gic_v2.h>
+# endif /* GIC */
+
+# define MAINTENANCE_IRQ 25
+# define SYSREGS_BASE	0x1c010000
+# define UART_BASE	0x1c090000
+
+#endif /* CONFIG_MACH_FOUNDATION_V8 */
+
 #endif /* !_JAILHOUSE_ASM_PLATFORM_H */
