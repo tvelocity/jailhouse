f4d162ee2b4f2a397c97839b250c8124744231b9
Antonios Motakis <antonios.motakis@huawei.com>
2015-08-07 14:08:32 +0200

    hypervisor: arm64: add percpu.h header file
    Add the percpu.h header file for the Aarch64 implementation. This is
    the bare bones version of the header needed to compile a stub hypervisor
    binary on Aarch64. A lot of these fields could probably be moved to an
    arch independent header.
    
    Signed-off-by: Antonios Motakis <antonios.motakis@huawei.com>

---
+++ hypervisor/arch/arm64/include/asm/percpu.h
@@ -0,0 +1,63 @@
+/*
+ * Jailhouse AArch64 support
+ *
+ * Copyright (C) 2015 Huawei Technologies Duesseldorf GmbH
+ *
+ * Authors:
+ *  Antonios Motakis <antonios.motakis@huawei.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#ifndef _JAILHOUSE_ASM_PERCPU_H
+#define _JAILHOUSE_ASM_PERCPU_H
+
+#include <jailhouse/types.h>
+#include <asm/paging.h>
+
+#define NUM_ENTRY_REGS			31
+
+/* Keep in sync with struct per_cpu! */
+#define PERCPU_SIZE_SHIFT		13
+#define PERCPU_STACK_END		PAGE_SIZE
+#define PERCPU_LINUX_SP			PERCPU_STACK_END
+
+#ifndef __ASSEMBLY__
+
+#include <asm/cell.h>
+#include <asm/spinlock.h>
+
+struct per_cpu {
+	/* common fields */
+	unsigned int cpu_id;
+	struct cell *cell;
+	u32 stats[JAILHOUSE_NUM_CPU_STATS];
+	int shutdown_state;
+	bool failed;
+
+	bool flush_vcpu_caches;
+} __attribute__((aligned(PAGE_SIZE)));
+
+static inline struct per_cpu *this_cpu_data(void)
+{
+	return NULL;
+}
+
+#define DEFINE_PER_CPU_ACCESSOR(field)					\
+static inline typeof(((struct per_cpu *)0)->field) this_##field(void)	\
+{									\
+	return this_cpu_data()->field;					\
+}
+
+DEFINE_PER_CPU_ACCESSOR(cpu_id)
+DEFINE_PER_CPU_ACCESSOR(cell)
+
+static inline struct per_cpu *per_cpu(unsigned int cpu)
+{
+	return NULL;
+}
+
+#endif /* !__ASSEMBLY__ */
+
+#endif /* !_JAILHOUSE_ASM_PERCPU_H */
