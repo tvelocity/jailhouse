2216e00ce2d983b62e679e1237201209d9633c01
Antonios Motakis <antonios.motakis@huawei.com>
2015-10-15 18:15:00 +0200

    hypervisor: arm64: plug the irqchip and GICv2 code from the AArch32
    We plug the irqchip handling code, and the GICv2 implementation from
    AArch32.
    
    GICv3 is slightly trickier; it makes heavier use of the sysregs, so
    we will need to review more carefully that the sysregs macros do
    the right thing.
    
    Signed-off-by: Antonios Motakis <antonios.motakis@huawei.com>

--- hypervisor/arch/arm64/control.c
+++ hypervisor/arch/arm64/control.c
@@ -12,6 +12,10 @@
 
 #include <jailhouse/control.h>
 #include <jailhouse/printk.h>
+#include <asm/control.h>
+#include <asm/irqchip.h>
+#include <asm/platform.h>
+#include <asm/traps.h>
 
 int arch_cell_create(struct cell *cell)
 {
@@ -80,4 +84,47 @@ void arch_panic_park(void)
 {
 	trace_error(-EINVAL);
 	while (1);
+}
+
+void arch_handle_sgi(struct per_cpu *cpu_data, u32 irqn)
+{
+	cpu_data->stats[JAILHOUSE_CPU_STAT_VMEXITS_MANAGEMENT]++;
+
+	switch (irqn) {
+	case SGI_INJECT:
+		irqchip_inject_pending(cpu_data);
+		break;
+	default:
+		printk("WARN: unknown SGI received %d\n", irqn);
+	}
+}
+
+unsigned int arm_cpu_virt2phys(struct cell *cell, unsigned int virt_id)
+{
+	return trace_error(-EINVAL);
+}
+
+unsigned int arm_cpu_phys2virt(unsigned int cpu_id)
+{
+	return trace_error(-EINVAL);
+}
+
+/*
+ * Handle the maintenance interrupt, the rest is injected into the cell.
+ * Return true when the IRQ has been handled by the hyp.
+ */
+bool arch_handle_phys_irq(struct per_cpu *cpu_data, u32 irqn)
+{
+	if (irqn == MAINTENANCE_IRQ) {
+		cpu_data->stats[JAILHOUSE_CPU_STAT_VMEXITS_MAINTENANCE]++;
+
+		irqchip_inject_pending(cpu_data);
+		return true;
+	}
+
+	cpu_data->stats[JAILHOUSE_CPU_STAT_VMEXITS_VIRQ]++;
+
+	irqchip_set_pending(cpu_data, irqn, true);
+
+	return false;
 }
